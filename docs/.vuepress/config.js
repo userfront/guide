d05a70fc62bbab8b766aa04bbb49616a5f97abd0
Tyler <tyler@anymod.com>
2020-02-25 08:46:59 -0800

    Initial setup

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,97 @@
+module.exports = {
+  title: "Userfront Guide",
+  description: "Documentation & Examples for Userfront",
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        href:
+          "https://res.cloudinary.com/component/image/upload/v1519346560/mod_logo_120px.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?family=Material+Icons"
+      }
+    ],
+    /* Production scripts */
+    [
+      "script",
+      { id: "AnyMod-script" },
+      "(function (m,o,d,u,l,a,r,i,z,e) {u[m]={Project:o,rq:[],Opts:r,ready:function(j){u[m].rq.push(j)}};function j(s){return encodeURIComponent(btoa(s))};z=l.getElementById(m+'-'+a);r=u.location;e=[d+'/page/'+o+'/'+j(r.pathname)+'/'+j(r.host)+'?t='+Date.now(),d];e.map(function(w){i=l.createElement(a);i.defer=1;i.src=w;z.parentNode.insertBefore(i,z);});})('AnyMod','8PP6M2','https://cdn.anymod.com/v2',window,document,'script', { toolkit: false });"
+    ]
+    /* Local scripts */
+    // ['script', { id: 'AnyMod-script' }, "(function (m,o,d,u,l,a,r,i,z,e) {u[m]={Project:o,rq:[],Opts:r,ready:function(j){u[m].rq.push(j)}};function j(s){return encodeURIComponent(btoa(s))};z=l.getElementById(m+'-'+a);r=u.location;e=['http://localhost:5000/v2/page/'+o+'/'+j(r.pathname)+'/'+j(r.host)+'?t='+Date.now(),d];e.map(function(w){i=l.createElement(a);i.defer=1;i.src=w;z.parentNode.insertBefore(i,z);});})('AnyMod','8PP6M2','http://localhost:4000/v2',window,document,'script', { toolkit: false });"],
+  ],
+  // plugins: [
+  //   [
+  //     "@vuepress/google-analytics",
+  //     {
+  //       ga: "UA-89033101-2"
+  //     }
+  //   ]
+  // ],
+  themeConfig: {
+    nav: [
+      { text: "Guide", link: "/guide/" },
+      { text: "Examples", link: "/examples/" }
+    ],
+    sidebar: {
+      "/guide/": [
+        "/guide/welcome.html",
+        "/guide/",
+        {
+          title: "Mod features",
+          collapsable: false,
+          children: ["/guide/welcome.html"]
+        },
+        {
+          title: "Mod editor",
+          collapsable: false,
+          children: ["/guide/welcome.html"]
+        },
+        {
+          title: "Using AnyMod",
+          collapsable: false,
+          children: ["/guide/welcome.md"]
+        }
+      ]
+      // "/examples/": [
+      //   ["/examples/", "List of examples"],
+      //   // Getting started
+      //   "/examples/add-a-mod.html",
+      // ],
+    },
+    lastUpdated: "Last Updated", // string | boolean
+    // https://vuepress.vuejs.org/default-theme-config/#algolia-search
+    // algolia: {
+    //   apiKey: "1869e5a6fd56b1d3d72d2e23acd8b900",
+    //   indexName: "anymod"
+    // },
+
+    // Assumes GitHub. Can also be a full GitLab url.
+    // repo: 'anymod/guide',
+    // repoLabel: 'Contribute',
+
+    // Optional options for generating "Edit this page" link
+
+    // if your docs are in a different repo from your main project:
+    // docsRepo: 'anymod/guide',
+    // if your docs are not at the root of the repo:
+    // docsDir: '..',
+    // if your docs are in a specific branch (defaults to 'master'):
+    docsBranch: "master",
+    // defaults to false, set to true to enable
+    editLinks: true,
+    // custom text for edit link. Defaults to "Edit this page"
+    editLinkText: "Help us improve this page"
+  },
+  extendMarkdown(md) {
+    return {
+      lineNumbers: true
+    };
+  }
+};
---
+++ docs/README.md
@@ -0,0 +1,26 @@
+---
+home: true
+heroImage: https://res.cloudinary.com/component/image/upload/v1519768558/mod_logo_min.svg
+actionText: Get Started →
+actionLink: /guide/
+footer: ©2020 Userfront
+---
+
+<div class="features">
+  <div class="feature">
+    <h2>Guide</h2>
+    <p>
+      Introduction, quickstart, and documentation.
+      <br>
+      <a href="/guide/">Browse the docs →</a>
+    </p>
+  </div>
+  <div class="feature">
+    <h2>Examples</h2>
+    <p>
+      Various examples of how to use Userfront.
+      <br>
+      <a href="/examples/">See examples →</a>
+    </p>
+  </div>
+</div>
