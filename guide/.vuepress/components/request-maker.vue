b98a0da5e5354ea8284c8f90faa5cea356fefe89
Tyler <tyler@anymod.com>
2021-04-05 11:10:26 -0700

    Adding request-maker

--- guide/.vuepress/components/request-maker.vue
+++ guide/.vuepress/components/request-maker.vue
@@ -20,8 +20,38 @@
         >
       </select>
     </form>
+    <div style="margin-bottom:12px;">
+      <el-button-group>
+        <el-button
+          size="small"
+          :type="isHeader('none') ? 'primary' : ''"
+          @click="setHeader('none')"
+          >No token</el-button
+        >
+        <el-button
+          size="small"
+          :type="isHeader('user') ? 'primary' : ''"
+          @click="setHeader('user')"
+          >Logged in user</el-button
+        >
+        <el-button
+          size="small"
+          :type="isHeader('admin') ? 'primary' : ''"
+          @click="setHeader('admin')"
+          >Admin user</el-button
+        >
+      </el-button-group>
+    </div>
+    <div>
+      <el-button-group>
+        <el-button type="primary" size="small">/public</el-button>
+        <el-button type="primary" size="small" plain>/protected</el-button>
+        <el-button type="primary" size="small" plain>/admin</el-button>
+      </el-button-group>
+    </div>
 
     <pre><code>fetch({
+  header: "{{ headerValue.value }}",
   route: "{{ routeValue }}"    
 })</code></pre>
   </div>
@@ -29,32 +59,49 @@
 
 <script>
 export default {
-  props: ["url"],
+  props: ["url", "headerType"],
   data() {
     return {
       urlValue: "",
-      headerValue: "Bearer ",
-      routeValue: "/public",
+      headerValue: {},
       headerValues: [
         {
           name: "No token",
+          type: "none",
           value: "",
         },
         {
           name: "Logged-in user",
+          type: "user",
           value: "eyreg",
         },
         {
           name: "Admin user",
+          type: "admin",
           value: "eyadmin",
         },
       ],
+      routeValue: "/public",
       routeValues: ["/public", "/protected", "/admin"],
     };
   },
+  methods: {
+    setHeader(type) {
+      if (!type) return;
+      this.headerValues.map((hv) => {
+        if (hv.type === type) {
+          this.headerValue = hv;
+        }
+      });
+    },
+    isHeader(type) {
+      console.log(this.headerValue.type, type);
+      return this.headerValue.type === type;
+    },
+  },
   mounted() {
     this.urlValue = this.url || "";
-    this.headerValue = this.header || this.headerValue;
+    this.setHeader(this.headerType);
   },
 };
 </script>
--- guide/.vuepress/enhanceApp.js
+++ guide/.vuepress/enhanceApp.js
@@ -1,4 +1,9 @@
-export default ({ router }) => {
+import "element-ui/lib/theme-chalk/index.css";
+import { Button, ButtonGroup } from "element-ui";
+
+export default ({ router, Vue }) => {
+  Vue.use(Button);
+  Vue.use(ButtonGroup);
   // Render mod after each route change
   router.afterEach(() => {
     try {
