bc67b10f2b860288cc85e5b53e90a153ec5c1ac5
Tyler <tyler@anymod.com>
2021-04-15 14:29:46 -0700

    Update directory

--- all/.vuepress/enhanceApp.js
+++ all/.vuepress/enhanceApp.js
@@ -1,14 +1,53 @@
-import "element-ui/lib/theme-chalk/index.css";
+import Vuex from "vuex";
+import store from "./utils/store.js";
+import axios from "axios";
 import { Button, ButtonGroup, Input } from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 
-export default ({ router, Vue }) => {
+const docsJsonUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5001/v0/docs.json"
+    : "https://api.userfront.com/v0/docs.json";
+
+export default async ({ router, Vue }) => {
+  // Add vuex
+  Vue.use(Vuex);
+  Vue.mixin({ store });
+
+  // Add Element UI components
   Vue.use(Button);
   Vue.use(ButtonGroup);
   Vue.use(Input);
+
   // Render mod after each route change
   router.afterEach(() => {
     try {
       if (window.Userfront) Userfront.render();
+      // scrollToAnchor();
     } catch (err) {}
   });
+
+  // Load the docs.json spec
+  try {
+    const { data } = await axios.get(docsJsonUrl);
+    Vue.prototype.$docs = data;
+    Vue.prototype.$demoToken =
+      "uf_test_readonly_demo1234_2d87b3d230bda5685276b43efdac2852";
+    store.dispatch("setActiveProject");
+  } catch (error) {
+    console.error("Problem fetching docs.json");
+    console.error(error);
+  }
+
+  // function scrollToAnchor() {
+  //   setTimeout(() => {
+  //     if (window.location.hash) {
+  //       const element = document.getElementById(window.location.hash.slice(1));
+
+  //       if (element) {
+  //         element.scrollIntoView();
+  //       }
+  //     }
+  //   }, 500);
+  // }
 };
