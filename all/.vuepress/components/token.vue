e2f30a8915688f4c00ed66b729c77f6c13ae9956
Tyler <tyler@anymod.com>
2021-08-13 11:14:17 -0700

    Token component working

--- all/.vuepress/components/token.vue
+++ all/.vuepress/components/token.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card light-code">
-    <h4>{{ title || 'Token' }}</h4>
+    <h4>{{ title || "Token" }}</h4>
     <div class="language-json extra-class">
       <pre
         class="language-json"
@@ -8,20 +8,47 @@
     </div>
     <el-button
       size="small"
-      @click="toggleValue"
+      type="text"
+      @click="showDecoded = !showDecoded"
       class="btn-decode-encode"
-    >Decode</el-button>
+      >{{
+        showDecoded ? "Hide decoded payload" : "Show decoded payload"
+      }}</el-button
+    >
+    <div class="language-json extra-class" v-show="showDecoded">
+      <pre
+        class="language-json"
+      ><code class="language-json">{{ decodedValue}}</code></pre>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   props: ["title", "value"],
+  data() {
+    return {
+      showDecoded: false,
+    };
+  },
+  computed: {
+    decodedValue() {
+      return this.getJWTPayload(this.value);
+    },
+  },
   methods: {
-    toggleValue() {
-
-    }
-  }
+    getJWTPayload(token) {
+      try {
+        const encodedPayload = token
+          .split(".")[1]
+          .replace("-", "+")
+          .replace("_", "/");
+        return JSON.parse(atob(encodedPayload));
+      } catch (error) {
+        console.error("Problem decoding JWT payload", error);
+      }
+    },
+  },
 };
 </script>
 
@@ -29,24 +56,28 @@ export default {
 .card {
   position: relative;
 }
+
 .card pre {
-  padding: 10px 0 12px;
-  margin: 0px 0 40px;
+  padding: 8px 6px;
+  margin: 4px 0 40px -6px;
 }
+
 /deep/ .code-copy {
   position: absolute;
   top: auto;
   bottom: 0;
   right: 0;
+
   i.material-icons {
     position: relative;
     top: 0;
     right: 0;
   }
 }
+
 .btn-decode-encode {
   position: absolute;
   z-index: 3;
-  bottom: 8px;
+  top: 78px;
 }
 </style>
